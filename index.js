e50df5ecadff9b2e84e8cc0534b56c017cd8cbde
Yoel88 <yosiasignatius88@gmail.com>
2021-10-13 10:11:22 +0700

    add revisi index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-function getFullname(firstName, lastName) {
+function isiPiringKu(makananPokok, sayuran, laukPauk, buahBuahan) {
   //process
-  const fullName = `${firstName} ${lastName}`;
+  const menu = `"Hari ini saya makan ${makananPokok} pakai ${sayuran} lalu ${laukPauk} serta ${buahBuahan}`;
 
   // nilai balikan
-  return fullName;
+  return menu;
 }
 
 // call the function
-console.log(getFullname("Yosias", "Ignatius"));
+console.log(
+  isiPiringKu(
+    "Nasi,",
+    "Tumis Brokoli,",
+    "Telur Rebus dan Tahu Kukus,",
+    "2 Buah Jeruk."
+  )
+);
