3f378b1f450ff3f037a8b1060a243e4a0067dc33
Agus Saputra Ntoi <ntoiagussaputra@gmail.com>
2021-10-18 14:01:14 +0700

    2nd assessment

---
+++ week2/index.js
@@ -0,0 +1,12 @@
+const removeFirstAndLastCharacter = (input) => {
+    // the process
+    return // the result
+}
+
+const reversedString = (input) => {
+    // the process
+    return // the result
+}
+
+console.log(removeFirstAndLastCharacter("malam")) // expected output : ala
+console.log(reversedString("siang")) // expected output : gnais
