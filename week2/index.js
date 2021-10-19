1057c18aefa55058ab71b88cbc0fe993031ee12e
Rahmat Adi Wijaya <adiraffael@gmail.com>
2021-10-19 08:36:53 +0700

    first commit

--- week2/index.js
+++ week2/index.js
@@ -1,11 +1,20 @@
 const removeFirstAndLastCharacter = (input) => {
     // the process
-    return // the result
+    let result = '';
+    for (let index = 0; index < input.length; index++) {
+        if (index === 0 || index === input.length - 1) continue
+        result += input[index];
+    }
+    return result// the result
 }
 
 const reversedString = (input) => {
     // the process
-    return // the result
+    let result = '';
+    for (let index = input.length - 1; index >= 0; index--) {
+        result += input[index];
+    }
+    return result// the result
 }
 
 console.log(removeFirstAndLastCharacter("malam")) // expected output : ala
