dc66b9462184d7d3bf8359f002e80454a8beaf27
Pranidya Luigi <pranidyaluigi@gmail.com>
2021-10-22 00:34:48 +0700

    Add assignment week 2

--- week2/index.js
+++ week2/index.js
@@ -1,12 +1,19 @@
 const removeFirstAndLastCharacter = (input) => {
-    // the process
-    return // the result
-}
+  let result = "";
+  for (let i = 0; i < input.length; i++) {
+    if (i === 0 || i === input.length - 1) continue;
+    result += input[i];
+  }
+  return result;
+};
 
 const reversedString = (input) => {
-    // the process
-    return // the result
-}
+  let result = "";
+  for (let i = input.length - 1; 0 <= i; i--) {
+    result += input[i];
+  }
+  return result;
+};
 
-console.log(removeFirstAndLastCharacter("malam")) // expected output : ala
-console.log(reversedString("siang")) // expected output : gnais
+console.log(removeFirstAndLastCharacter("malam"));
+console.log(reversedString("siang"));
