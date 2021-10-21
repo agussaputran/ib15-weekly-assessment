e869526bd2131ef0854af468ffc33bb98154cf9b
Agif12 <ahyarsagipul@gmail.com>
2021-10-21 14:56:14 +0800

    add weekly assestment sagipul ahyar

--- week2/index.js
+++ week2/index.js
@@ -1,12 +1,23 @@
 const removeFirstAndLastCharacter = (input) => {
-    // the process
-    return // the result
-}
+  // the process
+  let result = "";
+  for (let i = 0; i < input.length; i++) {
+    if (i == 0 || i == input.length - 1) continue;
+    result += input[i];
+  }
+  return result;
+  // the result
+};
 
 const reversedString = (input) => {
-    // the process
-    return // the result
-}
+  // the process
+  let result = "";
+  for (let i = input.length - 1; i >= 0; i--) {
+    result += input[i];
+  }
+  return result;
+  // the result
+};
 
-console.log(removeFirstAndLastCharacter("malam")) // expected output : ala
-console.log(reversedString("siang")) // expected output : gnais
+console.log(removeFirstAndLastCharacter("sagipul")); // expected output : agipu
+console.log(reversedString("ahyar")); // expected output : rayha
