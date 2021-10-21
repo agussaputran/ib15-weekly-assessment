40cfbadfba3aef94d57612ca10245296d0b0c4e5
Mishar <89523938+misharDH@users.noreply.github.com>
2021-10-21 14:38:51 +0800

    Add files via upload

---
+++ week2/index.js
@@ -0,0 +1,27 @@
+const removeFirstAndLastCharacter = (input) => {
+    // the process
+    let potong = "";
+    for (let i = 0; i < input.length; i++) {
+      if (i == 0 || i == input.length - 1) continue;
+      potong += input[i];
+    }
+    return potong;
+  };
+  
+  const reversedString = (input) => {
+    // the process
+    let newString = "";
+    for (let i = input.length - 1; i >= 0; i--) {
+      newString += input[i];
+    }
+    return newString;
+    // the result
+  };
+  
+  console.log(removeFirstAndLastCharacter("malam")); 
+  console.log(reversedString("siang")); 
+
+
+
+
+ 
