52384322b502236f1829b7636d595e1127a4f42d
Daniel Harianja <danieltogarharianja5@gmail.com>
2021-10-21 20:09:27 +0700

    tugasweek2

--- week2/index.js
+++ week2/index.js
@@ -1,12 +1,20 @@
 const removeFirstAndLastCharacter = (input) => {
-    // the process
-    return // the result
-}
+    let data = "";
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] != "m") {
+        data += input[i];
+        }
+    }
+    return data;
+};
 
 const reversedString = (input) => {
-    // the process
-    return // the result
-}
+    let data = "";
+    for (let j = input.length - 1; j >= 0; j--) {
+        data += input[j];
+    }
+    return data;
+};
 
-console.log(removeFirstAndLastCharacter("malam")) // expected output : ala
-console.log(reversedString("siang")) // expected output : gnais
+console.log(removeFirstAndLastCharacter("malam")); // expected output : ala
+console.log(reversedString("siang")); // expected output : gnais
