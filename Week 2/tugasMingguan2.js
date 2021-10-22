67ca7035a2bd59900c21c449156d3857b009010d
Yoel88 <yosiasignatius88@gmail.com>
2021-10-22 16:32:54 +0700

    tugasMingguan2

---
+++ Week 2/tugasMingguan2.js
@@ -0,0 +1,22 @@
+const removeFirstAndLastCharacter = (input) => {
+  // the process
+  let entry = "";
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] != "m") {
+      entry += input[i];
+    }
+  }
+  return entry; // the result
+};
+
+const reversedString = (input) => {
+  // the process
+  let entry = "";
+  for (let i = input.length - 1; i > 0; i--) {
+    entry += input[i];
+  }
+  return entry; // the result
+};
+
+console.log(removeFirstAndLastCharacter("malam")); // expected output : ala
+console.log(reversedString("siang")); // expected output : gnais
