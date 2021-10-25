3e5d31ebe95b22037058b84d2f796cce0f32d33e
Gerry Pratama <gerryajie@gmail.com>
2021-10-26 02:19:41 +0700

    Weekly tugas

--- week3/index.js
+++ week3/index.js
@@ -1,11 +1,14 @@
 function sumOfArray(arr1, arr2) {
-    let result = 0
-    // the logic 
-    // type the code below
+  let result = 0;
+  // the logic
+  // type the code below
+  let arrayBaru = arr1.concat(arr2);
+  let jumlah = (result = arrayBaru.reduce(function (akumulasi, valueNow) {
+    return akumulasi + valueNow;
+  }));
 
-    // type the code above
-    return result
+  return result;
 }
 
-console.log(sumOfArray([1,2,3,5], [1,2,3])) // expected output : 17
-console.log(sumOfArray([1,2,3], [1,2,3,2])) // expected output : 14
+console.log(sumOfArray([1, 2, 3, 5], [1, 2, 3])); // expected output : 17
+console.log(sumOfArray([1, 2, 3], [1, 2, 3, 2])); // expected output : 14
