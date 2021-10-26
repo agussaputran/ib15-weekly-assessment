5375d945504a0cf959afdbad5c4ba9c27003b733
Rahmat Adi Wijaya <adiraffael@gmail.com>
2021-10-26 11:21:52 +0700

    week 3

--- week3/index.js
+++ week3/index.js
@@ -1,11 +1,15 @@
 function sumOfArray(arr1, arr2) {
-    let result = 0
-    // the logic 
+    let result = 0;
+    // the logic
     // type the code below
+    let newArray = arr1.concat(arr2);
 
-    // type the code above
-    return result
-}
+    for (let i = 0; i < newArray.length; i++) {
+    result += newArray[i];
+    }
 
-console.log(sumOfArray([1,2,3,5], [1,2,3])) // expected output : 17
-console.log(sumOfArray([1,2,3], [1,2,3,2])) // expected output : 14
+    return result;
+  }
+  
+  console.log(sumOfArray([1, 2, 3, 5], [1, 2, 3])); // expected output : 17
+  console.log(sumOfArray([1, 2, 3], [1, 2, 3, 2])); // expected output : 14
