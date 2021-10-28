cb9ff6dc0d1cac51bae06ec1abb8d9b7838e4d9b
muhazi ramadhan <muhazisayapkiripmii@gmail.com>
2021-10-28 23:34:23 +0800

    tugas week3

--- week3/index.js
+++ week3/index.js
@@ -2,17 +2,11 @@ function sumOfArray(arr1, arr2){
                let result =  0;
                for(let i=0;i<arr1.length;i++)
                  {                  
-                   if(isNaN(arr1[i])){
-                   continue;
-                    }
                      result += Number(arr1[i]);
                   }
 
                 for(let j=0;j<arr2.length;j++)
-                 {                  
-                   if(isNaN(arr2[j])){
-                   continue;
-                    }
+                 {                    
                      result += Number(arr2[j]);
                   }
                 return result;
